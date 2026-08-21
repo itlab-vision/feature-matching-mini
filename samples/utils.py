c80fcf6645b408d49bc438f2663d3301ae52a2bd
SimonovDima <fdjgfdj722@gmail.com>
2026-08-22 02:18:08 +0300

    Support for new models has been added to the benchmark for quality and performance analysis

--- samples/utils.py
+++ samples/utils.py
@@ -190,9 +190,65 @@ def build_hpatches_task_config(args):
     return config
 
 
+def build_hpatches_detector_config(args):
+    config = dict()
+    if (args.device is not None) and (args.detector in DNN_DETECTORS):
+        config['device'] = args.device
+    if args.det_nfeatures is not None:
+        config['nfeatures'] = args.det_nfeatures
+    if args.det_noctave is not None:
+        config['nOctaveLayers'] = args.det_noctave
+    if args.det_threshold is not None:
+        config['threshold'] = args.det_threshold
+    return config
+
+
+def build_hpatches_descriptor_config(args):
+    config = dict()
+    if (args.device is not None) and (args.descriptor in DNN_DESCRIPTORS or args.descriptor in ['tfeat', 'hardnet']):
+        config['device'] = args.device
+    if args.des_nfeatures is not None:
+        config['nfeatures'] = args.des_nfeatures
+    if args.des_threshold is not None:
+        config['threshold'] = args.des_threshold
+    if args.des_scale is not None:
+        config['scale_factor'] = args.des_scale
+    return config
+
+
+def build_hpatches_matcher_config(args):
+    config = dict()
+    if (args.device is not None) and (args.matcher in DNN_MATCHERS):
+        config['device'] = args.device
+    if args.matcher in OPENCV_MATCHERS:
+        config['mode'] = args.matcher_mode
+    if args.mat_ratio is not None:
+        config['ratio'] = args.mat_ratio
+    if args.mat_cross_check is not None:
+        config['cross_check'] = args.mat_cross_check
+    return config
+
+
+def build_hpatches_preprocessor_config(args):
+    config = dict()
+    if args.device is not None:
+        config['device'] = args.device
+
+    return config
+
+
+def build_hpatches_feature_matcher_config(args):
+    return {
+        'detector': build_hpatches_detector_config(args),
+        'descriptor': build_hpatches_descriptor_config(args),
+        'matcher': build_hpatches_matcher_config(args),
+        'preprocessor': build_hpatches_preprocessor_config(args),
+    }
+
+
 def build_hpatches_benchmark_config(args):
     return {
-        **build_feature_matcher_config(args),
+        **build_hpatches_feature_matcher_config(args),
         "dataset": build_hpatches_dataset_config(args),
         "task": build_hpatches_task_config(args)
     }
